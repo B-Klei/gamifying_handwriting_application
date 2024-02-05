6720f9e8c94820ef7b0686858096d13a2388a600
B-Klei <kekadula@gmail.com>
2024-02-05 18:16:20 +0100

    commenting code, corrections

--- attribute.py
+++ attribute.py
@@ -1,8 +1,7 @@
 class Attribute:
     def __init__(self, std_limit, colour):
         self.std_limit = std_limit  # maximum standard deviation for which a badge is assigned
-        self.colour = colour  # attribute name
-        self.point_list = []  # list of earned points
+        self.colour = colour  # colour assigned for attribute
         self.points = 0  # points earned for the attribute
         self.point_list = []  # list of earned points
         self.badges = 0  # number of badges earned for the attribute
--- gamification.py
+++ gamification.py
@@ -1,10 +1,12 @@
 
 # Assigning points
 def assign_points(standard_deviation, std_limit, point_limit):
-    points = int(100 - (standard_deviation*100/(std_limit*100/point_limit)))
-    # "limit*100/20" = std for which 0 points are assigned
-    # assigns 100 points for std=0.0, 80 points for std=limit
-    if points < 0:
+    # point calculation formula
+    zero_points = std_limit*100/(100-point_limit)  # std for which 0 points are assigned
+    points = int(100 - (standard_deviation*100/zero_points))  # points calculated and rounded down
+    # 100 points assigned for "std = 0.0", "point_limit points" assigned for "std = std_limit"
+
+    if points < 0:  # avoiding negative points
         points = 0
 
     return points
@@ -12,20 +14,20 @@ def assign_points(standard_deviation, std_limit, point_limit):
 
 # Assigning badges
 def assign_badges(points, point_limit):
-    badge = 0
+    badge = 0  # establishing variable
 
-    if points >= (100-point_limit):
-        badge = 1
+    if points >= point_limit:  # if achieved points are above limit,
+        badge = 1  # badge is awarded
 
     return badge
 
 
 # Next goal
 def next_goal(achieved, goal_list):
-    i = 0
-    result = goal_list[0]
-    while achieved >= goal_list[i]:
-        result = goal_list[i+1]
+    i = 0  # iterating variable
+    result = goal_list[0]  # initial value = first value in the list
+    while goal_list[i] <= achieved:  # going through as long as the list item value is smaller than the achieved goal
+        result = goal_list[i+1]  # result is the next goal in the list
         i += 1
 
     return result
@@ -33,6 +35,7 @@ def next_goal(achieved, goal_list):
 
 # Progress bar
 def progress_bar(progress_points, full_points):
+    # calculating the achieved points portion in percentage
     progress_portion = 100*progress_points/full_points
 
     return progress_portion
